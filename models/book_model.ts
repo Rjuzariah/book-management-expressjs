20382a3c8f06157ce3bf15686681b3358fa9ae41
Juzariah <riajuzariah@gmail.com>
2024-10-17 12:13:55 +0700

    setup model dan database

---
+++ models/book_model.ts
@@ -0,0 +1,48 @@
+// book.model.ts
+import { DataTypes, Model } from 'sequelize';
+import sequelize from './database';
+
+class Book extends Model {
+  public id!: number;
+  public title!: string;
+  public author!: string;
+  public releaseYear!: number;
+  public genre!: string;
+  public description!: string;
+}
+
+Book.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    publishedYear: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    genre: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'books',
+  }
+);
+
+export default Book;

8c4be2a2158f6fd00803b9f08e6644c3479582c1
Juzariah <riajuzariah@gmail.com>
2025-01-21 16:50:55 +0700

    adding swagger and comment static token for easy debugging

--- index.ts
+++ index.ts
@@ -5,6 +5,10 @@ import bodyParser from 'body-parser';
 import authMiddleware from './middleware/authMiddleware'; // Import the auth middleware
 import seedBooks from "./models/seed_book";
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+
 const cors = require('cors');
 const dotenv = require('dotenv');
 
@@ -12,14 +16,19 @@ dotenv.config();
 
 const app: Express = express()
 const port = process.env.PORT;
+
 app.use(cors());
+
+
 
 // Middleware
 app.use(bodyParser.json());
 
 // Use the authentication middleware for all routes
-const expectedToken = "book-management-static-token"; // Define your static token
-app.use(authMiddleware(expectedToken));
+// const expectedToken = "book-management-static-token"; // Define your static token
+// app.use(authMiddleware(expectedToken));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 app.get('/', (req:Request, res:Response) => {
@@ -49,5 +58,9 @@ const startServer = async () => {
     console.log(`Server running on port ${port}`);
   });
 };
-
+// if (require.main === module) {
+//   startServer(); // Start the server if this file is run directly
+// }
 startServer(); // Start the server and run migrations
+
+export default app; // Export the app for testing
